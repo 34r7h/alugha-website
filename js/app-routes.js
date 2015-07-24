f48a54dab4115a95b05fec5ef73bd757925315bd
irthos <irth03@gmail.com>
2015-07-23 21:49:42 -0700

    all the pages of the main site complete via routing and directives. TODO: put the data in a service (hopefully gathering from a database), page carousels, image switching, and popovers.

--- js/app-routes.js
+++ js/app-routes.js
@@ -1,7 +1,37 @@
 (function() {
   'use strict';
-  angular.module('alughaHome').config(function($urlRouterProvider) {
-    return $urlRouterProvider.otherwise('/home');
+  angular.module('alugha').config(function($urlRouterProvider, $stateProvider) {
+    $stateProvider.state('alugha', {
+      url: '',
+      abstract: true,
+      template: '<div data-ui-view></div>'
+    }).state('alugha.disclaimer', {
+      url: '/disclaimer',
+      templateUrl: '/pages/disclaimer/disclaimer.tpl.html',
+      controller: 'DisclasimerCtrl',
+      controllerAs: 'disclaimer'
+    }).state('alugha.tos', {
+      url: '/tos',
+      templateUrl: '/pages/tos/tos.tpl.html',
+      controller: 'TosCtrl',
+      controllerAs: 'tos'
+    }).state('alugha.prices', {
+      url: '/prices',
+      templateUrl: '/pages/prices/prices.tpl.html',
+      controller: 'PricesCtrl',
+      controllerAs: 'prices'
+    }).state('alugha.jobs', {
+      url: '/jobs',
+      templateUrl: 'pages/jobs/jobs.tpl.html',
+      controller: 'JobsCtrl',
+      controllerAs: 'jobs'
+    }).state('alugha.jobs.developer', {
+      url: '/developer',
+      templateUrl: 'pages/jobs/developer/developer.tpl.html',
+      controller: 'DeveloperCtrl',
+      controllerAs: 'developer'
+    });
+    return $urlRouterProvider.otherwise('/');
   });
 
 }).call(this);
